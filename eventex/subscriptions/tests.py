2192eb39e2a73b2373e1b376b851797b49ff67c4
Eduardo de Matos <eduardosilva@hermes.com.br>
2013-08-01 19:15:54 -0300

    criar testes e html do formulario padrao

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -18,3 +18,10 @@ class SubscribeTest(TestCase):
 
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'subscriptions/inscricao.html')
+
+    def test_html(self):
+        'Html must contain input controls.'
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 5)
+        self.assertContains(self.resp, 'type="text"', 4)
+        self.assertContains(self.resp, 'type="submit"')
