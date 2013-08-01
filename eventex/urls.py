6af5875ef64a39ac1b4f8c3ae18d10c28ac63870
Eduardo Matos <edu.de.matos@hotmail.com>
2013-07-31 22:21:54 -0300

    implementacao da view basica de inscricao

--- eventex/urls.py
+++ eventex/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('eventex.core.views',
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'eventex.views.home', name='home'),
     # url(r'^eventex/', include('eventex.foo.urls')),
@@ -14,5 +14,6 @@ urlpatterns = patterns('eventex.core.views',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'homepage', name='homepage'),
+    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.inscricao', name='inscricao')
 )
