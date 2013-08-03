39d214a4c17fa1f588c0f47e171e3aab4f351b6a
Eduardo Matos <edu.de.matos@hotmail.com>
2013-08-03 12:43:47 -0300

    testes para o form, refatoracao da view

--- eventex/subscriptions/tests/tests_views_subscribe.py
+++ eventex/subscriptions/tests/tests_views_subscribe.py
@@ -1,5 +1,5 @@
 from eventex.subscriptions.forms import SubscriptionForm
-
+from eventex.subscriptions.models import Subscription
 from django.test import TestCase
 
 
@@ -34,3 +34,34 @@ class SubscribeTest(TestCase):
         'Form must have 4 fields'
         form = self.resp.context['form']
         self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Eduardo de Matos', cpf='11122233380', email='edu@matos.com', phone='21-32456789')
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        'Valid post should redirect to /inscricao/1/'
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_save_info(self):
+        self.assertTrue(Subscription.objects.exists())
+
+
+class SubscribeInvalidPostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Eduardo de Matos', cpf='123456789012', email='edu@matos.com', phone='21-32456789')
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        'Invalid post should not redirect'
+        self.assertNotEqual(302, self.resp.status_code)
+
+    def test_form_errors(self):
+        'Form must contain errors'
+        self.assertTrue(self.resp.context['form'].errors)
+
+    def test_dont_save(self):
+        'Do not save data'
+        self.assertFalse(Subscription.objects.exists())
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,25 @@
 # Create your views here.
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def inscricao(request):
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return new(request)
+
+def new(request):
     return render(request, 'subscriptions/inscricao.html', {'form': SubscriptionForm()})
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'subscriptions/inscricao.html', {'form': form})
+
+    obj = form.save()
+    return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
+
+
