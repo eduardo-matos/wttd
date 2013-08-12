f24d81240b5b1a47f6d4ea65ab31800a5af034e6
Eduardo de Matos <eduardosilva@hermes.com.br>
2013-08-12 18:53:37 -0300

    finalizacao do model Contact

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -10,4 +10,19 @@ class Speaker(models.Model):
     description = models.TextField(_('Descrição'), blank=True)
 
     def __unicode__(self):
-    	return self.name
+        return self.name
+
+
+class Contact(models.Model):
+    KINDS = (
+        ('E',_('E-mail')),
+        ('P',_('Telefone')),
+        ('F',_('Fax')),
+    )
+
+    speaker = models.ForeignKey('Speaker', verbose_name=_('palestrante'))
+    kind = models.CharField(_('tipo'), max_length=1, choices=KINDS)
+    value = models.CharField(_('valor'), max_length=255)
+
+    def __unicode__(self):
+        return self.value
--- eventex/core/tests/test_models.py
+++ eventex/core/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from eventex.core.models import Speaker
+from eventex.core.models import Speaker, Contact
 
 class SpeakerModelTest(TestCase):
     def setUp(self):
@@ -15,3 +15,50 @@ class SpeakerModelTest(TestCase):
 
     def test_unicode(self):
     	self.assertEqual('Eduardo Matos', unicode(self.speaker))
+
+
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name = 'Eduardo Matos',
+            url = 'http://eduardodematos.com',
+            description = 'desenvolvedor',
+            slug = 'eduardo-matos'
+        )
+
+    def test_email(self):
+        contato = Contact.objects.create(
+            speaker = self.speaker,
+            kind = 'E',
+            value = 'edu@matos.com'
+        )
+
+        self.assertEqual(1, Contact.objects.all().count())
+
+    def test_phone(self):
+        contato = Contact.objects.create(
+            speaker = self.speaker,
+            kind = 'P',
+            value = '21-22554477'
+        )
+
+        self.assertEqual(1, Contact.objects.all().count())
+
+    def test_faz(self):
+        contato = Contact.objects.create(
+            speaker = self.speaker,
+            kind = 'F',
+            value = 'edu@matos.com'
+        )
+
+        self.assertEqual(1, Contact.objects.all().count())
+
+    def test_unicode(self):
+        "Contact unicode must be it's value"
+        contato = Contact.objects.create(
+            speaker = self.speaker,
+            kind = 'F',
+            value = 'edu@matos.com'
+        )
+
+        self.assertEqual('edu@matos.com', unicode(contato))
