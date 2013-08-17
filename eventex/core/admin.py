273f0facced8a9ff4568111e436db5c6119f600e
Eduardo Matos <edu.de.matos@hotmail.com>
2013-08-17 12:48:09 -0300

    permitir adicao de Medias aos Talks na tela de admin

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from eventex.core.models import Contact, Speaker, Talk
+from eventex.core.models import Contact, Speaker, Talk, Media
 
 class ContactInline(admin.TabularInline):
     model = Contact
     extra = 1
 
+class MediaInline(admin.TabularInline):
+    model = Media
+    etra = 1
+
 class SpeakerAdmin(admin.ModelAdmin):
     inlines = [ContactInline,]
     prepopulated_fields = {'slug': ('name',)}
 
+class TalkAdmin(admin.ModelAdmin):
+    inlines = [MediaInline,]
+
 admin.site.register(Speaker, SpeakerAdmin)
-admin.site.register(Talk)
+admin.site.register(Talk, TalkAdmin)
